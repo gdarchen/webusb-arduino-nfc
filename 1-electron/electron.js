1f430d6c840535c22fd1001d2ccf034990dee166
Gautier Darchen <darchen.gautier@gmail.com>
2019-10-31 10:08:15 +0100

    refactor: clean electron.js a bit

--- 1-electron/electron.js
+++ 1-electron/electron.js
@@ -1,41 +1,23 @@
 const electron = require("electron");
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const { app } = electron;
+const { BrowserWindow } = electron;
 
 const path = require("path");
 const isDev = require("electron-is-dev");
-const fs = require("fs");
 
-process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = "true";
 
 let mainWindow;
-
-// Build the absolute path to the React Dev Tool Chrome Extension
-// function buildPathToReactDevTool() {
-//     const chromeAppRoot = '%LOCALAPPDATA%\\Google\\Chrome\\User Data\\Default\\Extensions\\fmkadmapgofadopljbjfkapdkoienihi'.replace(
-//         /%([^%]+)%/g,
-//         function(_, n) {
-//             return process.env[n];
-//         }
-//     );
-//     let version = '';
-//     fs.readdirSync(chromeAppRoot).forEach((file) => (version = file));
-//     return `${chromeAppRoot}\\${version}`;
-// }
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 2736,
     height: 1824,
     show: false,
-    icon: __dirname + "./icons/icon.png"
+    icon: `${__dirname}./icons/icon.png`
   });
 
-  if (isDev) {
-    // mainWindow.maximize();
-    // Add the React Dev Tool extension
-    // BrowserWindow.addDevToolsExtension(buildPathToReactDevTool());
-  } else {
+  if (!isDev) {
     mainWindow.setFullScreen(true);
   }
 
@@ -51,7 +33,9 @@ function createWindow() {
       : `file://${path.join(__dirname, "./build/index.html")}`
   );
 
-  mainWindow.on("closed", () => (mainWindow = null));
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 }
 
 app.on("ready", createWindow);
