67e7c26ad7d872f6cc02593c57001a2c1decb9b7
Gautier Darchen <darchen.gautier@gmail.com>
2019-11-11 15:27:05 +0100

    feat(electron): add credits component

--- 1-electron/src/pages/home/components/Home.jsx
+++ 1-electron/src/pages/home/components/Home.jsx
@@ -1,26 +1,28 @@
+import PropTypes from "prop-types";
 import React from "react";
 import { Card, Loader } from "semantic-ui-react";
+
+import Credits from "../../../common/credits";
 import style from "./Home.scss";
 
-import PropTypes from "prop-types";
-
 const Home = ({ readTag }) => {
-  return readTag ? (
+  return (
     <div className={style.container}>
-      <Card className={style.nfcCard}>
-        <Card.Content>
-          <Card.Header>NFC Tag Read</Card.Header>
-          <Card.Description>
-            Received value: <div className={style.readValue}>{readTag}</div>
-          </Card.Description>
-        </Card.Content>
-      </Card>
-    </div>
-  ) : (
-    <div className={style.waitingContainer}>
-      <Loader active inverted inline="centered">
-        Waiting for a scan
-      </Loader>
+      {readTag ? (
+        <Card className={style.nfcCard}>
+          <Card.Content>
+            <Card.Header>NFC Tag Read</Card.Header>
+            <Card.Description>
+              Received value: <div className={style.readValue}>{readTag}</div>
+            </Card.Description>
+          </Card.Content>
+        </Card>
+      ) : (
+        <Loader active inverted inline="centered">
+          Waiting for a scan
+        </Loader>
+      )}
+      <Credits />
     </div>
   );
 };
