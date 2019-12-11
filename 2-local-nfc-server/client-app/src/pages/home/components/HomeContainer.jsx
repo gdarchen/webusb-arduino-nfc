4e92a4c184bbd8e12f576cacf0ce6e518c74b681
Gautier Darchen <darchen.gautier@gmail.com>
2019-12-11 17:15:25 +0100

    fix(client-app-websocket): retrigger useEffect when socket API URL changes

--- 2-local-nfc-server/client-app/src/pages/home/components/HomeContainer.jsx
+++ 2-local-nfc-server/client-app/src/pages/home/components/HomeContainer.jsx
@@ -10,7 +10,7 @@ const HomeContainer = () => {
 
   useEffect(() => {
     setSocket(SocketIO.connect(APILocalSocketUrl));
-  }, []);
+  }, [APILocalSocketUrl]);
 
   useEffect(() => {
     if (!socket) {
