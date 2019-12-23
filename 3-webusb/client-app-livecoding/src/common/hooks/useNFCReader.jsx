f8ad5f7e99f217472892a671175ee3f644b4115f
Gautier Darchen <darchen.gautier@gmail.com>
2019-12-23 09:19:02 +0100

    feat(livecoding): set a skeleton for useNFCReader hook

--- 3-webusb/client-app-livecoding/src/common/hooks/useNFCReader.jsx
+++ 3-webusb/client-app-livecoding/src/common/hooks/useNFCReader.jsx
@@ -5,59 +5,13 @@ const useNFCReader = () => {
   const [readTag, setReadTag] = useState();
 
   // Loop to read the NFC tags
-  const readLoop = async () => {
-    try {
-      if (device.opened) {
-        // Read the bytes from the device
-        const payload = await device.transferIn(5, 64);
-        const decoder = new TextDecoder();
-        const decodedPayload = decoder.decode(payload.data).split("/")[1];
+  const readLoop = async () => {};
 
-        setReadTag(decodedPayload);
-        console.log(`Read NTAG213 containing '${decodedPayload}'.`);
-
-        if (device) {
-          readLoop();
-        }
-      }
-    } catch (e) {
-      console.error(
-        `Error while reading from the NFC Reader:\n${e.toString()}`
-      );
-      throw e;
-    }
-  };
-
-  const setupNFCReader = async () => {
-    try {
-      if (device) {
-        await device.open();
-        await device.selectConfiguration(1);
-        await device.claimInterface(2);
-        await device.controlTransferOut({
-          requestType: "class",
-          recipient: "interface",
-          request: 0x22,
-          value: 0x01,
-          index: 0x02
-        });
-
-        readLoop();
-      }
-    } catch (e) {
-      console.error(`Error while setting up the NFC Reader:\n${e.toString()}`);
-    }
-  };
+  const setupNFCReader = async () => {};
 
   // Get the plugged devices and extract the one having the Arduino's vendor id, then asks the user
   // to choose from the resulting list
-  const configureNewNFCReader = async () => {
-    const authorizedDevice = await navigator.usb.requestDevice({
-      filters: [{ vendorId: 0x2341 }]
-    });
-
-    setDevice(authorizedDevice);
-  };
+  const configureNewNFCReader = async () => {};
 
   // When a device has been found, we initiate a connection with it
   useEffect(() => {
