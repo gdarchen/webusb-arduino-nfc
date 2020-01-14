e41e53f9b81cdf7dc8eaff8691a21766e709bde7
Gautier Darchen <darchen.gautier@gmail.com>
2020-01-14 14:59:32 +0100

    refactor(electron): remove magic numbers

--- 1-electron/src/pages/home/components/HomeContainer.jsx
+++ 1-electron/src/pages/home/components/HomeContainer.jsx
@@ -10,9 +10,12 @@ const HomeContainer = () => {
   nfc.on("reader", reader => {
     console.log(`${reader.reader.name}  device attached`);
 
+    const pageNumber = 4;
+    const bufferLength = 48;
+
     reader.on("card", card => {
       console.log(`${reader.reader.name}  card detected`, card);
-      reader.read(6, 24).then(data => {
+      reader.read(pageNumber, bufferLength).then(data => {
         // Data is encoded between slashes such a 'blibli/Hello Paris!/blabla' and we just want to
         // extract 'Hello Paris!'
         const extractedPayload = data.toString().split("/")[1];
