edae9b14c9df3477b2e6c54f66aa4071c5a1cc8d
Gautier Darchen <darchen.gautier@gmail.com>
2020-01-15 09:37:17 +0100

    fix(webusb-client): remove magic numbers and define const instead

--- 3-webusb/client-app/src/common/hooks/useNFCReader.jsx
+++ 3-webusb/client-app/src/common/hooks/useNFCReader.jsx
@@ -4,12 +4,23 @@ const useNFCReader = () => {
   const [device, setDevice] = useState();
   const [readTag, setReadTag] = useState();
 
+  const ARDUINO_VENDOR_ID = 0x2341;
+  const IN_ENDPOINT = 5; // In endpoint ID of WebUSB for Arduino (4 for Out endpoint)
+  const CONFIG_NUMBER = 1; // Device specific configuration value
+  const INTERFACE_NUMBER = 2; // Device specific interface number
+  const REQUEST_TYPE = 'class'; // industry-standard class of devices
+  const TRANSFER_RECIPIENT = "interface"; // target on the transfer on the device
+  const ARDUINO_CORE_REQUEST = 0x22; // see 'USBCore.h' > #define CDC_SET_CONTROL_LINE_STATE	0x22
+  const ARDUINO_CONTROL_CONNECT = 0x01; // Vendor-specific, 0x00 for DISCONNECT
+  const RECIPIENT_INTERFACE_NUMBER = 0x02; // Interface number on the recipient
+
   // Loop to read the NFC tags
   const readLoop = async () => {
     try {
       if (device.opened) {
         // Read the bytes from the device
-        const payload = await device.transferIn(5, 64);
+        const bufferLength = 64;
+        const payload = await device.transferIn(IN_ENDPOINT, bufferLength);
         const decoder = new TextDecoder();
         const decodedPayload = decoder.decode(payload.data).split("/")[1];
 
@@ -30,14 +41,14 @@ const useNFCReader = () => {
     try {
       if (device) {
         await device.open();
-        await device.selectConfiguration(1);
-        await device.claimInterface(2);
+        await device.selectConfiguration(CONFIG_NUMBER);
+        await device.claimInterface(INTERFACE_NUMBER);
         await device.controlTransferOut({
-          requestType: "class",
-          recipient: "interface",
-          request: 0x22,
-          value: 0x01,
-          index: 0x02
+          requestType: REQUEST_TYPE, 
+          recipient: TRANSFER_RECIPIENT,
+          request: ARDUINO_CORE_REQUEST,
+          value: ARDUINO_CONTROL_CONNECT,
+          index: RECIPIENT_INTERFACE_NUMBER
         });
 
         readLoop();
@@ -52,7 +63,7 @@ const useNFCReader = () => {
   // to choose from the resulting list
   const configureNewNFCReader = async () => {
     const authorizedDevice = await navigator.usb.requestDevice({
-      filters: [{ vendorId: 0x2341 }]
+      filters: [{ vendorId: ARDUINO_VENDOR_ID }]
     });
 
     setDevice(authorizedDevice);
